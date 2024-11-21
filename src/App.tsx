5b6c4c26e3b7fc2798270dd7b681ce52319b4596
Sabyr <tolomushev_s@iuca.kg>
2024-11-21 10:24:01 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Blog} from './components/Blog'
+
+function App() {
+
+ return (
+  <div className="App">
+   <div className="navbar">
+    <ul>
+     <li>Home</li>
+     <li>Blog</li>
+    </ul>
+   </div>
+    <Blog />
+  </div>
+ )
+}
+export default App
